1920e793479145ec50649688d25b234c0b603d1b
Sithum Wickramanayaka <92084117+suwickramanayaka@users.noreply.github.com>
2024-08-12 00:14:22 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Sithum Uthsara on 2024-08-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
